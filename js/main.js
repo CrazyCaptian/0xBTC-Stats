bc37471b6600c23396e4230d37a7e90a1299aa75
CrazyCaptian <dottage@gmail.com>
2022-01-01 04:48:46 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -37,7 +37,7 @@ const _MINING_TARGET_INDEX = '11';
 const _MAXIMUM_TARGET_BN = new Eth.BN(_MAXIMUM_TARGET_STR, 10);
 const _MINIMUM_TARGET_BN = new Eth.BN(_MINIMUM_TARGET);
 const _IDEAL_BLOCK_TIME_SECONDS = _ETH_BLOCKS_PER_REWARD * _SECONDS_PER_ETH_BLOCK;
-
+console.log("FUK");
 /* TODO: figure out why it doesn't work w metamask */
 var eth = new Eth(new Eth.HttpProvider("https://eth-mainnet.alchemyapi.io/v2/1R3-aP9q9pvu1aIPKhtLSkAZKgee_FUp"));
 // if (typeof window.web3 !== 'undefined' && typeof window.web3.currentProvider !== 'undefined') {
